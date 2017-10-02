2cea861ebd0c1a3fbab092975972a2986fa7f198
Kristian Covic <kristian.covic0903@gmail.com>
2017-10-02 20:42:08 +0200

    Comments added for Position parameters

--- src/smims/networking/model/Position.java
+++ src/smims/networking/model/Position.java
@@ -1,9 +1,9 @@
 package smims.networking.model;
 
 public class Position {
-	private CharacterStatus status;
-	private int index;
-	private int distanz;
+	private CharacterStatus status;		//Status eines Characters im Spiel: in der Base, auf dem Feld (Field) oder im Zielhaus (House)
+	private int index;					//Absolute Position des Charakters auf dem Spielfeld: In der Basis und im Haus -1, auf dem Spielfeld hat das Startfeld des ersten Spielers den Wert 0, ab da wird aufwärts gezählt
+	private int distanz;				//Distanz des Spielers von seinem Spawnfeld, ist auch innerhalb des Hauses gültig (Wenn das Spielfeld 40 Felder hat, startet der Spieler bei distance = 0, distance = 40 ist dann der erste Platz im Haus, 41 ist der zweite,...)
 	
 	public int getDistanz() {
 		return distanz;
@@ -17,7 +17,7 @@ public class Position {
 	{
 		status = CharacterStatus.BASE;
 		distanz = 0;
-		index = 0;
+		index = -1;
 	}
 
 	public CharacterStatus getStatus() {
