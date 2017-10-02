6e10f5796f726d609b824e4dcf336771759ce4df
Nils Moog <nils@2moog.local>
2017-10-02 15:26:31 +0200

    Player Class

--- src/smims/networking/model/Player.java
+++ src/smims/networking/model/Player.java
@@ -1,5 +1,44 @@
 package smims.networking.model;
 
 public class Player {
-
+	
+	int playerId;
+	Dice dice ;
+	Board myBoard;
+	
+	
+	/**
+	 * 
+	 * @param pPlayerID ID des Spielers
+	 * @param pBoard das verwendete Spielbrett
+	 */
+	Player(int pPlayerID, Board pBoard)
+	{
+		playerId = pPlayerID;
+		dice = new Dice();
+		myBoard = pBoard;
+	}
+	
+	/**
+	 * wuerfelt den wuerfel
+	 */
+	public void rollDice()
+	{
+		dice.diceRoll();
+	}
+	
+	/**
+	 * bewegt die Spielfigur and der gewaehlten position.
+	 * @param posCharacter Position der Spielfigur, die bewegt werden soll.
+	 */
+	public void moveCharacters(int posCharacter)
+	{
+		allCharacters = myBoard.getAllCharacters();
+		
+		
+		for(int i = 0; i< allCharacters ;i++)
+		board.moveCharacters(selectedCharacter, dice.getResult());
+	}
+	
+	
 }
