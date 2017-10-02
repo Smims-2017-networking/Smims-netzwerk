93067358107bc1422257b5e3e6e493e5fafd2478
smimsLukas <lukiwichmann@web.de>
2017-10-02 19:23:11 +0200

    Rundenmanager updated

--- src/smims/networking/model/Rundenmanager.java
+++ src/smims/networking/model/Rundenmanager.java
@@ -3,65 +3,62 @@ package smims.networking.model;
 import java.util.ArrayList;
 
 public class Rundenmanager {
-	
-	private ArrayList<Player> allPlayers;
-	
-	
-	
-	public Rundenmanager(ArrayList <Player> allPlayers)
-	{
+
+	private final ArrayList<Player> allPlayers;
+	private final ArrayList<IPlayerChoice> allChoices;
+
+	public Rundenmanager(ArrayList<Player> allPlayers, ArrayList<IPlayerChoice> allChoices) {
 		this.allPlayers = allPlayers;
+		this.allChoices = allChoices;
 	}
 
 	/**
 	 * Diese Methode sorgt für die Ausführung einer Spielrunde
 	 */
-	public void round()
-	{
-		//Dies wird für jeden Player in der Reihenfolge des Beitretens durchgeführt
-				for(Player p:allPlayers)
-				{
-					if(p.charactersCanMove()==0)
-					{
-						this.doThreeTurns(p);
-					}
-					
-					else
-					{
-						turn(p);	
-					}
-					//bei einer 6 darf nochmal gezogen werden
-					while(p.myDice.getResult()==6)
-					{
-						turn(p);
-					}
-				}
+	public void round() {
+		// Dies wird für jeden Player in der Reihenfolge des Beitretens
+		// durchgeführt
+		for (int i=0; i<allPlayers.size(); i++) 
+		{
+			Player p = allPlayers.get(i);
+			IPlayerChoice iPlayer = allChoices.get(i);
+			
+			if (p.charactersCanMove() == 0) {
+				this.doThreeTurns(p);
+			}
+
+			else {
+				turn(p,iPlayer);
+			}
+			// bei einer 6 darf nochmal gezogen werden
+			while (p.myDice.getResult() == 6) {
+				turn(p, iPlayer);
+			}
+		}
 	}
-	
-	
+
 	/**
 	 * fuehrt einen Zug für einen beliebigen Spieler aus: wuerfeln und ziehen
+	 * 
 	 * @param Player p ist der Spieler
 	 */
-	public void turn(Player p)
-	{
+	private void turn(Player p, IPlayerChoice iPlayer) {
 		p.rollDice();
-		int x=2;//get from PlayerChoice after implemented
+		int x = iPlayer.chooseCharacter;	//Feld, auf dem die zu bewegende Figur steht
 		p.moveCharacters(x);
 	}
-	
+
 	/**
-	 * Diese Methode führt bis zu drei Züge aus, bis der Spieler eine sechs gewürfelt hat
+	 * Diese Methode führt bis zu drei Züge aus, bis der Spieler eine sechs
+	 * gewürfelt hat
+	 * 
 	 * @param Player p ist der Spieler
 	 */
-	
-	public void doThreeTurns(Player p)
-	{
-		for(int i=0;i<3;i++)
-		{
+
+	public void doThreeTurns(Player p) {
+		for (int i = 0; i < 3; i++) {
 			turn(p);
-			if(p.myDice.getResult()==6)
-			{
+			if (p.myDice.getResult() == 6) {
 				break;
 			}
 		}
