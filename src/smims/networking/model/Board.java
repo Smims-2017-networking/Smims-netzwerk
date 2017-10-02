86ea298b3a11c1e9f3ca4ce706e40d2101e1150f
Nils Moog <nils@2moog.local>
2017-10-02 19:32:19 +0200

    Board und Position mit kommentaren

--- src/smims/networking/model/Board.java
+++ src/smims/networking/model/Board.java
@@ -20,6 +20,11 @@ public class Board implements IBoard {
 		}
 	}
 
+	/**
+	 * Checht ob auf dem Feld ein Character steht
+	 * @param pFieldNumber Nummer des zu ueberpruefenden Feldes
+	 * @return true wenn ein Character auf dem Feld ist
+	 */
 	public boolean fieldOccupied(int pFieldNumber) {
 		for (ICharacter currentCharacter : charactersOnBoard) {
 			if (currentCharacter.getPosition().getIndex() == pFieldNumber
@@ -30,6 +35,11 @@ public class Board implements IBoard {
 		return false;
 	}
 
+	/**
+	 * 
+	 * @param pFieldNumber
+	 * @return Character an der Steller auf dem Feld, wenn keiner vorhanden null
+	 */
 	public ICharacter getCharacterAt(int pFieldNumber) {
 		for (ICharacter currentCharacter : charactersOnBoard) {
 			if (currentCharacter.getPosition().getIndex() == pFieldNumber
@@ -40,41 +50,74 @@ public class Board implements IBoard {
 		return null;
 	}
 
+	/**
+	 * 
+	 * @param pCharacter
+	 * @param pDistance
+	 * @return returns true, wenn an der Stelle die der Character nach dem bewegen um pDistance auf dem gleichen Feld wie ein Character vom gleichen Team ist.
+	 */
+	public boolean characterWouldHitTeammate(ICharacter pCharacter, int pDistance)
+	{
+		if(pCharacter.getPosition().getStatus() == CharacterStatus.BASE)
+		{
+			pDistance = 0;
+		}
+		for(ICharacter TestCharacter: charactersOnBoard)
+		{
+			if(pCharacter.getPosition().getDistanz() + pDistance == TestCharacter.getPosition().getDistanz())
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	
+	/**
+	 * 
+	 * @param character	Character, der bewegt werden soll
+	 * @param distance Distanz, die der Character bewegt werden soll
+	 */
 	@Override
 	public void moveCharacter(ICharacter character, int distance) throws Exception{
 		
-		if(character.getPosition().getStatus() == CharacterStatus.BASE)					// Spieler in der Basis
+		int characterSpawnPosition = character.getPlayer().getPlayerId() * this.DistanceBetweenSpawns; 	
+		int possibleNewPosition = (character.getPosition().getIndex() + distance) % (DistanceBetweenSpawns * PlayerCount);
+		int possibleNewDistanz = character.getPosition().getDistanz() + distance;
+		
+		if(characterWouldHitTeammate(character, distance))
+		{
+			throw new MoveNotAllowedException();
+		}
+		else if(character.getPosition().getStatus() == CharacterStatus.BASE)					// Spieler in der Basis
 		{
 			if(distance == 6) {
-				int characterSpawnPosition = character.getPlayer().getPlayerId() * this.DistanceBetweenSpawns; 	
-			
-				if(fieldOccupied(characterSpawnPosition)){
-					if(getCharacterAt(characterSpawnPosition).getPlayer().getPlayerId() == character.getPlayer().getPlayerId()){
-					throw new MoveNotAllowedException();
-					} else {
-						character.getPosition().setIndex(characterSpawnPosition);
-						character.getPosition().setDistanz(character.getPosition().getDistanz()+distance);
-					}
-			
-				} else {
 					character.getPosition().setIndex(characterSpawnPosition);
 					character.getPosition().setDistanz(character.getPosition().getDistanz()+distance);
-				}
+				
 			} else {
 				throw new MoveNotAllowedException();
 			}
 		}
 		else
 		{
-			int possibleNewDistanz = character.getPosition().getDistanz() + distance;
-			int possibleNewPosition = (character.getPosition().getIndex() + distance) % (DistanceBetweenSpawns * PlayerCount);
-			
 			if(possibleNewDistanz > (DistanceBetweenSpawns * PlayerCount))		//muss in das Haus rein oder ist im Haus drin.
 			{
-				if(possibleNewDistanz - (DistanceBetweenSpawns * PlayerCount) > CharactersPerPlayer || )	//Platz an der Stelle im Haus?
+				if(possibleNewDistanz - (DistanceBetweenSpawns * PlayerCount) > CharactersPerPlayer)	
 				{
 					throw new MoveNotAllowedException();
 				}
+				else
+				{
+					character.getPosition().setDistanz(character.getPosition().getDistanz()+distance);
+					character.getPosition().setStatus(CharacterStatus.HOUSE);
+					character.getPosition().setIndex(-1);
+				}
+			}
+			else{
+				character.getPosition().setDistanz(character.getPosition().getDistanz()+distance);
+				character.getPosition().setIndex(possibleNewPosition);
 			}
 		}
 		
@@ -82,6 +125,9 @@ public class Board implements IBoard {
 		
 	}
 
+	/**
+	 * returns
+	 */
 	@Override
 	public Collection<IReadonlyCharacter> getAllCharachters() {
 		Collection<IReadonlyCharacter> characters = new ArrayList<IReadonlyCharacter>();
