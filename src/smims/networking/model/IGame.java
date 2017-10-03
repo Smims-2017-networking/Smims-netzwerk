ee651638984591abb17f13458052b7aab35f4b2d
Kristian Covic <kristian.covic0903@gmail.com>
2017-10-03 11:55:50 +0200

    Added playerID attribute to void rollDice

--- src/smims/networking/model/IGame.java
+++ src/smims/networking/model/IGame.java
@@ -3,7 +3,7 @@ package smims.networking.model;
 import java.util.ArrayList;
 
 public interface IGame {	
-	void rollDice();
+	void rollDice(int playerID);
 	public int getDiceResult();
 	public Board getBoard();
 	
