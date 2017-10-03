c154be7054224a2f4eb455980e658bef8c17b1d1
Alexander Hoischen <alexander.hoischen@gmx.net>
2017-10-03 16:02:09 +0200

    Implement Turn

--- src/smims/networking/model/Turn.java
+++ src/smims/networking/model/Turn.java
@@ -5,7 +5,7 @@ public class Turn {
 	private final IBoard board;
 	private final DiceRoller diceRoller;
 	private int rolledCount = 0;
-	private int mustRollCount = 0;
+	private int maxRollCount = 1;
 	private TurnState turnState;
 	
 	
@@ -19,16 +19,55 @@ public class Turn {
 		return player;
 	}
 	
-	public void moveCharacter(int characterPos) {
-		
+	public void moveCharacter(int characterPos) throws MoveNotAllowedException {
+		EnsurePlayerCanMoveCharacter();
+		moveCharacterAtPosition(characterPos);
+		if (diceRoller.getResult() == 6) {
+			transitionToRollingState();
+		} else {
+			transitionToFinishedState();
+		}
 	}
 	
+	private void moveCharacterAtPosition(int characterPos) {
+		board.moveCharacter(iCharacter, distance);
+	}
+
+	private void EnsurePlayerCanMoveCharacter() throws MoveNotAllowedException {
+		if (turnState != TurnState.ExpectMove) {
+			throw new MoveNotAllowedException();
+		}
+		
+	}
+
+	private void transitionToRollingState() {
+		turnState = TurnState.ExpectRoll;
+		
+	}
+
+	private void transitionToFinishedState() {
+		turnState = TurnState.Finished;
+		
+	}
+
 	public void rollDice() throws MoveNotAllowedException {
 		EnsurePlayerCanRollDice();
+		diceRoller.rollDice();
+		rolledCount++;
+		if ((maxRollCount != 1 && diceRoller.getResult() == 6) || maxRollCount == 1) {
+			transitionToMoveState();
+		} else if (maxRollCount == rolledCount) {
+			transitionToFinishedState();
+		}
+	}
+
+	private void transitionToMoveState() {
+		turnState = TurnState.ExpectMove;
+		
 	}
 
 	private void EnsurePlayerCanRollDice() throws MoveNotAllowedException {
-		if(!(turnState == TurnState.ExpectRoll) && rolledCount < mustRollCount) {
+		if(!(turnState == TurnState.ExpectRoll) && rolledCount < maxRollCount) {
 			throw new MoveNotAllowedException();
 		}
 	}
@@ -39,15 +78,5 @@ public class Turn {
 	
 	boolean isOver() {
 		return turnState == TurnState.Finished;
-	}
-	
-	private void doThreeTurns(Player p) throws Exception {
-		for (int i = 0; i < 3; i++) {
-			diceRoller.rollDice();
-			if (gameState.myDice.getResult() == 6) {
-				turnWithoutRoll(p);
-				break;
-			}
-		}
 	}
 }
