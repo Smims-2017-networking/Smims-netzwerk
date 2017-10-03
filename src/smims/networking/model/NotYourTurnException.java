47da62cb178e257032f126e53c1939993296257a
Nils Moog <nils@2moog.local>
2017-10-03 11:03:45 +0200

    GameState

---
+++ src/smims/networking/model/NotYourTurnException.java
@@ -0,0 +1,10 @@
+package smims.networking.model;
+
+public class NotYourTurnException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7746719863781216049L;
+	
+}
