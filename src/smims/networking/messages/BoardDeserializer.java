bf1f3db4079677e3e762ac574d8a6a45f5ab9673
Alexander Hoischen <alexander.hoischen@gmx.net>
2017-10-05 21:52:25 +0200

    Fix gson, maybe

--- src/smims/networking/messages/BoardDeserializer.java
+++ src/smims/networking/messages/BoardDeserializer.java
@@ -4,6 +4,7 @@ import java.lang.reflect.*;
 import java.util.Collection;
 
 import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 
 import smims.networking.model.Board;
 import smims.networking.model.BoardDescriptor;
@@ -14,7 +15,7 @@ public class BoardDeserializer implements JsonDeserializer<Board>{
 	public Board deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext jsonContext) throws JsonParseException {
 		return new Board(
 				jsonContext.deserialize(json.getAsJsonObject().get("boardDescriptor"), BoardDescriptor.class),
-				jsonContext.deserialize(json.getAsJsonObject().get("charactersOnBoard"), Collection.class),
+				jsonContext.deserialize(json.getAsJsonObject().get("charactersOnBoard"), new TypeToken<Collection<Character>>(){}.getClass()),
 				json.getAsJsonObject().get("boardSectionSize").getAsInt());
 	}
 }
